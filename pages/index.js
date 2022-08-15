630961a0ea5c2316d1c31710751509f96581751f
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2022-08-15 12:56:34 -0300

    stylesheet running

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import Head from 'next/head';
+
 function Home() {
-    return <div>Welcome to Authenticator Page!</div>
+    return (
+    <div>
+
+        <Head>
+            <meta charSet='utf-8' />
+            <meta name='robots' content='index, follow' />
+            <meta name='description' content="Site sobre ... " />
+            <meta name='author' content=' Matheus Authenticator clone' />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css' />
+            <title>Home</title>
+        </Head>
+        Welcome to Authenticator Page!
+    </div>
+        )
   }
   
   export default Home;
