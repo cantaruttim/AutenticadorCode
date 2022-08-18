b2c9c4030569539271498e49cb1b9b39c6f43d27
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2022-08-18 08:37:07 -0300

    background color and cdn

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ba87ffec0bc2f16"; }
+/******/ 		__webpack_require__.h = function() { return "172083d9b05f2301"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
